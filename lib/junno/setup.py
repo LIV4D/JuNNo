05b707cf6399b795ee9c52373c10f5f2bf3775fb
ClementPla <clement.playout@polymtl.ca>
2019-02-13 16:11:28 +0900

    Add setup.py for quick install

---
+++ lib/junno/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='JuNNo',
+    version='',
+    packages=['junno', 'junno.nnet', 'junno.j_utils', 'junno.j_utils.ipython',
+              'junno.j_utils.sql_tools', 'junno.datasets'],
+    url='',
+    license='',
+    author='Gabriel',
+    author_email='',
+    description=''
+)
