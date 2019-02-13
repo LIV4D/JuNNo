27a2c0012a5877fccc65fb53416a92a28afc228d
ClementPla <clement.playout@polymtl.ca>
2019-02-13 16:11:39 +0900

    Add setup.py for quick install

--- lib/junno/setup.py
+++ lib/junno/setup.py
@@ -9,5 +9,17 @@ setup(
     license='',
     author='Gabriel',
     author_email='',
-    description=''
+    description='',
+    requires=['ipywidgets(>=7.4.2)',
+              'sympy(>=1.3)',
+              'tqdm(>=4.29.1)',
+              'numpy(>=1.15.4)',
+              'traitlets(>=4.3.2)',
+              'six(>=1.12.0)',
+              'scipy(>=1.1.0)',
+              'pandas(>=0.23.4)',
+              'psutil(>=5.4.8)',
+              'ipython>=7.2.0)',
+              'scikit_learn(>=0.20.2)',
+              'tables(>=3.4.4)']
 )
